f45376fe0b25068cc037825cc7d4df6b9c3ab168
Alexandre Bitar <joabitar76@gmail.com>
2021-03-10 11:56:47 -0300

    "tarefa": adicionado componente de lista de repositórios

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>AlexBitar80</strong>
+          <p>Descrição do repositório</p>
+
+          <a href="">Acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>AlexBitar80</strong>
+          <p>Descrição do repositório</p>
+
+          <a href="">Acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>AlexBitar80</strong>
+          <p>Descrição do repositório</p>
+
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
