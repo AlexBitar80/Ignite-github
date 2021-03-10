a1a2983e8c356f18979c2f76bbb72d74b413deb1
Alexandre Bitar <joabitar76@gmail.com>
2021-03-10 10:39:40 -0300

    "tarefa": adicionado novas configurações do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
