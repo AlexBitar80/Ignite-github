09f026a75d93531645e6f48a1d381c2efac77fb7
Alexandre Bitar <joabitar76@gmail.com>
2021-03-11 12:25:17 -0300

    "tarefa": adicionado tipagem para o estado de reposítorios

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,9 +1,15 @@
-import { RepositoryItem } from "./RepositoryItem";
+import { RepositoryItem, RepositoryItemProps } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useEffect, useState } from "react";
 
+type Repository = {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/users/AlexBitar80/repos')
