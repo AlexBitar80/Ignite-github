51af6913baacd9460bb590992cc3825cec4940e6
Alexandre Bitar <joabitar76@gmail.com>
2021-03-11 10:30:05 -0300

    "tarefa": adicionado uso dos efeitos para pegar dados da API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { useEffect, useState } from "react";
 
 
 const repository = {
@@ -9,6 +10,14 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/AlexBitar80/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
